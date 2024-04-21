39bb5184d0f5716f5a77e94219573fb9035f852d
DenSad81 <tagil81@mail.ru>
2024-04-21 06:16:10 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        string stopWord = "exit";
+        string inputWord = " ";
+
+        Console.WriteLine("Вы попали в день сурка, чтоб он закончился ведите стоп слово");
+
+        while (inputWord != stopWord)
+        {
+            Console.Write("Введите стоп слово: ");
+            inputWord = (Console.ReadLine());
+            Console.WriteLine("День сурка продолжается.");
+        }
+
+        Console.WriteLine("Ура, день сурка окончен!");
+    }
+}
+
